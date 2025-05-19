cb6d0316153b31a0602ced4407e6b7bb87462812
Anirudh Suresh <anirudh05suresh@gmail.com>
2025-05-19 11:26:44 +0530

    Add files via upload

---
+++ f.js
@@ -0,0 +1,8 @@
+let rur= document.getElementById('welcome');
+
+function invisible(){
+    setTimeout(() => {
+        rur.style.display='none';
+      }, 6000); // 4000 ms = 4 seconds
+}
+invisible();
